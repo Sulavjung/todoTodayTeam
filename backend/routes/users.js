a7935a309dd6f62e6eb02e1ad3cb8d28014b767d
Sulavjung <79497159+Sulavjung@users.noreply.github.com>
2024-02-26 10:25:58 -0800

    Team

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require("../config/database");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET team data. */
+router.get("/", async function (req, res, next) {
+  try {
+    const [teamData, _] = await db.execute("SELECT * FROM team");
+
+    // Sending team data in JSON format
+    res.json(teamData);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [teamData, setTeamData] = useState([]);
+
+  useEffect(() => {
+    fetch("54.219.136.176/teams")
+      .then((response) => response.json())
+      .then((data) => setTeamData(data))
+      .catch((error) => console.error("Error fetching team data:", error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <header className="App-header">
+          <h1>Team TodoToday</h1>
+        </header>
+        <main>
+          <div className="team-list">
+            {teamData.map((member) => (
+              <div key={member.id} className="team-member">
+                <img
+                  className="profImage"
+                  src={`https://api.multiavatar.com/${encodeURIComponent(
+                    member.name
+                  )}.png`}
+                  alt=""
+                />
+                <h2>{member.name}</h2>
+                <p>
+                  <strong>Email:</strong> {member.email}
+                </p>
+                <p>
+                  <strong>Role:</strong> {member.role}
+                </p>
+                <p>
+                  <strong>Hobby:</strong> {member.hobby}
+                </p>
+              </div>
+            ))}
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
